c6c66ebc9b1b0cc53ac8f24454d6711eeb6e93c5
Ensar Sarajčić <es.ensar@gmail.com>
2021-03-26 11:03:26 +0100

    Fix formatting and lint issues

--- lua/vimwiki_reviews/templates.lua
+++ lua/vimwiki_reviews/templates.lua
@@ -5,45 +5,45 @@ local api = require('vimwiki_reviews.vimwiki_api')
 
 local M = {}
 
--- Returns an object with template functions 
+-- Returns an object with template functions
 -- matching syntax of vimwiki with passed index
-function M.for_vimwiki(vimwiki_index) 
-	local syntax = api.get_vimwiki_syntax(vimwiki_index)
-	return M.for_syntax(syntax)
+function M.for_vimwiki(vimwiki_index)
+  local syntax = api.get_vimwiki_syntax(vimwiki_index)
+  return M.for_syntax(syntax)
 end
 
 -- Returns an object with template functions matching passed syntax
-function M.for_syntax(syntax) 
-	local syntaxed = {}
+function M.for_syntax(syntax)
+  local syntaxed = {}
 
-	-- Returns string representing header of passed level in correct syntax
-	function syntaxed.header(level, text)
-		local template = api.get_vimwiki_syntaxlocal('rxH' .. level .. '_Template', syntax)
-		local result = template:gsub('__Header__', text)
-		return result
-	end
+  -- Returns string representing header of passed level in correct syntax
+  function syntaxed.header(level, text)
+    local template = api.get_vimwiki_syntaxlocal('rxH' .. level .. '_Template', syntax)
+    local result = template:gsub('__Header__', text)
+    return result
+  end
 
-	-- Returns string representing link with passed link and text in correct syntax
-	function syntaxed.link(link, text)
-		local template = vim.fn['vimwiki#vars#get_global']('WikiLinkTemplate2')
-		if syntax == 'markdown'
-			then
-			template = api.get_vimwiki_syntaxlocal('Weblink1Template', syntax)
-		end
-		local result = template:gsub('__LinkUrl__', link):gsub('__LinkDescription__', text)
-		return result
-	end
+  -- Returns string representing link with passed link and text in correct syntax
+  function syntaxed.link(link, text)
+    local template = vim.fn['vimwiki#vars#get_global']('WikiLinkTemplate2')
+    if syntax == 'markdown'
+    then
+      template = api.get_vimwiki_syntaxlocal('Weblink1Template', syntax)
+    end
+    local result = template:gsub('__LinkUrl__', link):gsub('__LinkDescription__', text)
+    return result
+  end
 
-	-- Returns string representing list item with passed text in correct syntax
-	function syntaxed.list_item(text)
-		local bullet = vim.fn['vimwiki#lst#default_symbol']() .. ' '
-		local margin = vim.fn['vimwiki#lst#get_list_margin']()
-		local item_margin = string.rep(' ', margin)
+  -- Returns string representing list item with passed text in correct syntax
+  function syntaxed.list_item(text)
+    local bullet = vim.fn['vimwiki#lst#default_symbol']() .. ' '
+    local margin = vim.fn['vimwiki#lst#get_list_margin']()
+    local item_margin = string.rep(' ', margin)
 
-		return item_margin .. bullet .. text
-	end
+    return item_margin .. bullet .. text
+  end
 
-	return syntaxed
+  return syntaxed
 end
 
 return M
--- lua/vimwiki_reviews/vimwiki_api.lua
+++ lua/vimwiki_reviews/vimwiki_api.lua
@@ -13,39 +13,39 @@ local M = {}
 -- 2 = second vimwiki
 -- etc..
 function M.normalize_vimwiki_index(vimwiki_index)
-	if (vimwiki_index == 0)
-		then
-		local current_index = vim.fn['vimwiki#vars#get_bufferlocal']('wiki_nr')
-		if (current_index < 0)
-			then
-			current_index = 0
-		end
-		return current_index + 1
-	else
-		return vimwiki_index
-	end
+  if (vimwiki_index == 0)
+  then
+    local current_index = vim.fn['vimwiki#vars#get_bufferlocal']('wiki_nr')
+    if (current_index < 0)
+    then
+      current_index = 0
+    end
+    return current_index + 1
+  else
+    return vimwiki_index
+  end
 end
 
 -- Gets syntax of vimwiki based on passed index
 function M.get_vimwiki_syntax(vimwiki_index)
-	vimwiki_index = M.normalize_vimwiki_index(vimwiki_index)
-	return vim.g.vimwiki_wikilocal_vars[vimwiki_index]['syntax']
+  vimwiki_index = M.normalize_vimwiki_index(vimwiki_index)
+  return vim.g.vimwiki_wikilocal_vars[vimwiki_index]['syntax']
 end
 
 -- Gets extension of vimwiki based on passed index
 function M.get_vimwiki_extension(vimwiki_index)
-	vimwiki_index = M.normalize_vimwiki_index(vimwiki_index)
-	return vim.g.vimwiki_wikilocal_vars[vimwiki_index]['ext']
+  vimwiki_index = M.normalize_vimwiki_index(vimwiki_index)
+  return vim.g.vimwiki_wikilocal_vars[vimwiki_index]['ext']
 end
 
 -- Gets syntax local variable from vimwiki
 -- Useful for taking templates from vimwiki
 function M.get_vimwiki_syntaxlocal(key, syntax)
-	if not syntax
-		then
-		syntax = vim.fn['vimwiki#vars#get_wikilocal']['syntax']
-	end
-	return vim.fn['vimwiki#vars#get_syntaxlocal'](key, syntax)
+  if not syntax
+  then
+    syntax = vim.fn['vimwiki#vars#get_wikilocal']['syntax']
+  end
+  return vim.fn['vimwiki#vars#get_syntaxlocal'](key, syntax)
 end
 
 
